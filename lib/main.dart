90d099f684bd035fde13dd978b470722b4579d7a
apple <apple@appledeMacBook-Pro.local>
2018-08-10 18:17:25 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _saved = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  int _newList = 1;
 
   Widget _buildSuggestions() {
     return new ListView.builder(
@@ -97,6 +98,9 @@ class RandomWordsState extends State<RandomWords> {
           return new Scaffold(
             appBar: new AppBar(
               title: new Text('Saved Suggestions'),
+              actions: <Widget>[
+                new IconButton(icon: new Icon(Icons.next_week), onPressed: () {Navigator.push(context, new MaterialPageRoute(builder: (context) => new NewPage()));},)
+              ]
             ),
             body: new ListView(children: divided),
           );
@@ -105,6 +109,47 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
+  Widget _button (int num) {
+    return new Text('click me ${num.toString()}');
+  }
+
+  void _newPage () {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('this is a new page'),
+            ),
+            body: new Column(
+              children: <Widget>[
+                new Container(
+                  child: new Center(
+                    child: new FlatButton(
+                      child: new Text('ahaha', style: new TextStyle(color: Colors.white),),
+                      color: Colors.red,
+                      onPressed: () {
+                        setState(() {
+                          _newList += 1;         
+                        });
+                      },
+                    ),
+                  ),
+                  height: 100.0,
+                  decoration: new BoxDecoration(color: Colors.blue[500])
+                ),
+                new Expanded(
+                  child: new Center(
+                  ),
+                )
+              ],
+            )
+          );
+        }
+      )
+    );
+  }
+  
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -114,7 +159,84 @@ class RandomWordsState extends State<RandomWords> {
           new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
         ],
       ),
-      body: _buildSuggestions(),
+      body: new Column(
+        children: <Widget>[
+          new Container(
+            child: new Center(
+              child: new RaisedButton(
+                child: _button(_newList),
+                color: Colors.blue,
+                textColor: Colors.white,
+                onPressed: () {
+                  setState(() {
+                    _newList += 2;
+                  });
+                },
+              ),
+            ),
+            height: 50.0,
+          ),
+          new Expanded(
+            child:  _buildSuggestions(), 
+          )
+        ]
+      ),
+    );
+  }
+}
+
+
+
+class NewPage extends StatefulWidget {
+  @override
+ createState() => new NewPageState();
+}
+
+class NewPageState extends State<NewPage> {
+  final _list = [1, 2, 3, 4, 5];
+
+  @override
+  Widget build(BuildContext context) {
+    final tiles = _list.map(
+      (item) {
+        return new ListTile(
+          title: new Text(
+            item.toString(),
+          ),
+        );
+      },
+    );
+    final divided = ListTile
+      .divideTiles(
+        context: context,
+        tiles: tiles,
+      )
+      .toList();
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('test')
+      ),
+      body: new Column(
+        children: <Widget>[
+          new Container(
+            child: new Center(
+              child: new FlatButton(
+                child: new Text('click'),
+                onPressed: () {
+                  setState(() {
+                    _list.add(_list.length + 1);
+                  });
+                },
+              ),
+            ),
+          ),
+          new Expanded(
+            child: new ListView(
+              children: divided,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
